16d2f404c5daabc548d23d07654cae37f5591bc6
CHOSION <chosion0107@gmail.com>
2021-12-23 11:16:11 +0900

    211223
    2_oNoFF_ImageApp

---
+++ oNoFF/oNoFF/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  oNoFF
+//
+//  Created by 조세연 on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var imgOn: UIImage!
+    var imgOff: UIImage!
+
+    @IBOutlet var imgView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "on.jpg")
+        imgOff = UIImage(named: "off.jpg")
+    }
+
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
+    
+}
+
