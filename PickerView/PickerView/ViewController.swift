33e0d145d1d225ee4bc3be0ac923f149ab4ad805
CHOSION <chosion0107@gmail.com>
2022-01-01 15:03:55 +0900

    220101_5

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -7,14 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    var imageArray = [UIImage?]()
+    var imageFileName = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg"]
 
     @IBOutlet var pickerImage: UIPickerView!
     @IBOutlet var lblImageFileName: UILabel!
     @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in 0..<MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageFileName[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
     }
 
 
