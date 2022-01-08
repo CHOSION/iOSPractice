9012ca0ad64734e2738598a38ef6abada0db2923
CHOSION <chosion0107@gmail.com>
2022-01-09 00:16:03 +0900

    220109_1

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func tapNumberButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapDotButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapDeleteButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapChangeSignButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapPlusButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapMinusButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapMultipleButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapDivideButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapPercentButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapEqualButton(_ sender: Any) {
+    }
+    
 }
 
