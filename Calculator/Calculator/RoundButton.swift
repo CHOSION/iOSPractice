33ffc37d4f9256d7ff6dc2757343e0efbd2f4db4
CHOSION <chosion0107@gmail.com>
2022-01-08 23:24:51 +0900

    220108_3

--- Calculator/Calculator/RoundButton.swift
+++ Calculator/Calculator/RoundButton.swift
@@ -12,7 +12,7 @@ class RoundButton: UIButton {
     @IBInspectable var isRound: Bool = false {
         didSet {
             if isRound {
-                self.layer.cornerRadius = self.frame.height/2
+                self.layer.cornerRadius = self.frame.height/3
             }
         }
     }
