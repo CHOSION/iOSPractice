65e2824b7e5fbfa692b16e378ca0967a11e0a1ce
CHOSION <chosion0107@gmail.com>
2022-01-08 23:07:06 +0900

    220108_1

---
+++ Calculator/Calculator/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by 조세연 on 2022/01/08.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 조세연 on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
