99c026fd43ab370ad9acebb72d44e556d60d2466
CHOSION <chosion0107@gmail.com>
2022-01-08 23:23:36 +0900

    220108_2

--- Calculator/Calculator/RoundButton.swift
+++ Calculator/Calculator/RoundButton.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
+@IBDesignable
 class RoundButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height/2
+            }
+        }
     }
-    */
 
 }
