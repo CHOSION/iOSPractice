e5203d96f838969b6ff4a250c6b918b1caecc477
CHOSION <chosion0107@gmail.com>
2022-01-09 15:55:17 +0900

    220109_2

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,24 +7,42 @@
 
 import UIKit
 
-
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = "" // numberOutputLabel이 표시되는 프로퍼티
+    var firstOperand = "" // 이전계산값을 저장하는 프로퍼티
+    var secondOperand = "" // 새로 입력된 계산값을 저장하는 프로퍼티
+    var result = "" // 계산 결과값을 저장하는 프로퍼티
+    var currentOperation: Operation = .unknown // 현재 계산기에 어떤 연산자가 입력되어있는지 알수 있도록 연산자의 값을 저장하는 프로퍼티
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     
-    @IBAction func tapNumberButton(_ sender: Any) {
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
     }
     
-    @IBAction func tapClearButton(_ sender: Any) {
+    @IBAction func tapClearButton(_ sender: UIButton) {
     }
     
-    @IBAction func tapDotButton(_ sender: Any) {
+    @IBAction func tapDotButton(_ sender: UIButton) {
     }
     
     @IBAction func tapDeleteButton(_ sender: Any) {
